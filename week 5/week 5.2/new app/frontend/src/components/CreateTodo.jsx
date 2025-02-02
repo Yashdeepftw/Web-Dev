18bb86106054670dc3d78bcdaa728694db0007e5
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-02-02 11:54:31 +0530

    the connection with the backend is added

--- week 5/week 5.2/new app/frontend/src/components/CreateTodo.jsx
+++ week 5/week 5.2/new app/frontend/src/components/CreateTodo.jsx
@@ -1,18 +1,44 @@
-
+import { useState } from "react";
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return <div>
         <input style={{
           padding:20,
           margin:10
-        }} type="text" placeholder="title" /> <br />
+        }} onChange={(e) => {
+            const value = e.target.value;
+            setTitle(value);
+        }} 
+        type="text" placeholder="title" /> <br />
         <input style={{
             padding:10,
             margin: 10
-        }} type="text" placeholder="description" /><br />
+        }} onChange={(e) => {
+            const value = e.target.value;
+            setDescription(value);
+        }} 
+        type="text" placeholder="description" /><br />
 
         <button style={{
             padding:10,
             margin: 10
+        }}  
+        onClick={() => {
+            fetch("hthp://localhost:3000/todo",{
+                method: "POST",
+                body:  JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+                .then(async (res) => {
+                    const json = await res.json();
+                    alert("todo added")
+                })
         }}>Add a Todo</button>
     </div>
 }
