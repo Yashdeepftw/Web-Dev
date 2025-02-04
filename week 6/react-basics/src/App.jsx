4c19c2b27cb74e4f7d06877f23da7d896f496ca2
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-02-04 22:47:03 +0530

    added the code from the class 6.1

--- week 6/react-basics/src/App.jsx
+++ week 6/react-basics/src/App.jsx
@@ -5,51 +5,43 @@ import './App.css'
 import React from 'react'
 
 function App() {
-  const [title, setTitle] = useState("Random number")
 
-  function updateTitle() {
-    setTitle("My name is : " + Math.random());
-  }
-  return (
-    <div>
-      <button style={{
-        padding:10,
-      }} onClick={updateTitle}>update the title</button>
-      <Header title={title}></Header>
-      <Header title="My name is : Harkirat Singh"></Header>
-      <Header title="My name is : Harkirat Singh"></Header>
-      <Header title="My name is : Harkirat Singh"></Header>
+  const [todos, setTodos] = useState([]);
+  useEffect( () => {
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(async (res) {
+      const json = await res.json();
+      setTodos(json);
+    })
+  }, [])
+  
 
-    {/* <HeaderWithButton></HeaderWithButton>  
-    <Header title="My name is : Harkirat Singh"></Header> */}
+  return <div>
+    {todos.map(todo => <Todo title={todo.title} description={todo.description} />)}
     </div>
-  )
+  
 }
 
-// use this component function for re-rendering 
+function Todo({title, description}) {
+  return <div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+  </div>
+}
 
-// function HeaderWithButton () {
-//   const [title, setTitle] = useState("random number");
 
-//   function updateTodo() {
-//     setTitle("My title is : " + Math.random());
-//   }
-//   return <>
-//     <button onClick={updateTodo}>Update Todo by clicking</button>
-//     <Header title={title}></Header>
-//   </>
-// }
-
-// function Header({title}) {
+// function TextComponent() {
 //   return <div>
-//     {title}
+//     hi there
 //   </div>
 // }
 
-const Header = React.memo(function ({title}) {
-  return <div>
-    {title}
-  </div>
-})
+// function CardWapper({innerComponent}) {
+//   return <div style={{
+//     border:"2px solid black"
+//   }}>
+//     {innerComponent}
+//   </div>
+// }
 
 export default App
