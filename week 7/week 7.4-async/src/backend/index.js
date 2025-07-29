f53eecb6f02fba4151ed98b5e7cff5b6289b9e99
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-07-29 23:18:28 +0530

    pushed the basic code for the week 7.4-async class concepts with as personal backend server that send random notifications count everytime request is send

---
+++ week 7/week 7.4-async/src/backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+app.listen(3000);
+app.use(express.json());
+app.use(cors());
+
+app.get('/notifications', (req, res) => {
+    console.log("this is form server");
+
+    const notifications = {
+        network: Math.floor(Math.random() * 11),
+
+        jobs: Math.floor(Math.random() * 11),
+
+        messaging: Math.floor(Math.random() * 7),
+
+        notifications: Math.floor(Math.random() * 100)
+    };
+
+    res.json(notifications);
+})
