d6e44464489010649260d9db94b5fb206ce13aec
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-01 01:11:17 +0530

    learned about the tailwind usage and how to use className of different classes

--- week 8/Dukan-tailwind/src/App.jsx
+++ week 8/Dukan-tailwind/src/App.jsx
@@ -4,9 +4,13 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      
+    <>
+    <div className='grid grid-cols-3 md:grid-cols-1'>
+      <div className='bg-blue-500'>hi there</div>
+      <div className='bg-yellow-500'>hi there</div>
+      <div className='bg-green-500'>hi there</div>
     </div>
+    </>
   )
 }
 
