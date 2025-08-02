ecdf5eb4799bc7b60a1d8764e63925f6892434a0
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-03 01:39:11 +0530

    added the db connection and routes for the main router

---
+++ week 8/paytm-clone/backend/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { string } = require('zod');
+
+mongoose.connect("mongodb+srv://yashdeepftw:yashdeep_0218@paytm-clone.vrfdxlo.mongodb.net/");
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("connected to the MongoDB"); 
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    lastName: String,
+    firstName: String
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = {
+    User
+}
---
+++ week 8/paytm-clone/backend/index.js
@@ -0,0 +1,7 @@
+const { User } = require("./db");
+const express = require("express");
+const app = express();
+
+const mainRouter = require('./routes/index');
+
+app.use('api/v1', mainRouter);
