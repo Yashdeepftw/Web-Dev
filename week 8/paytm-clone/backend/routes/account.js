3f1f912e287ad2c27a3c33966f2cb0ef15a5d2e3
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-05 01:44:04 +0530

    added the account balance endpoint

--- week 8/paytm-clone/backend/routes/account.js
+++ week 8/paytm-clone/backend/routes/account.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const { User, Account } = require('../db');
+const authMiddleware = require('../middlewares/middlewares')
+
+
+router.get('balance', authMiddleware, async (req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+
+    res.status(200).json({
+        balance: account.balance
+    })
+})
 
 module.exports = {
     router
