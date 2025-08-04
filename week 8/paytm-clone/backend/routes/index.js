b25bc6730b84412298a561f7828bcdc575d4538b
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-05 01:35:46 +0530

    added the accountRouter in the index.js of the routes

--- week 8/paytm-clone/backend/routes/index.js
+++ week 8/paytm-clone/backend/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require('./user');
+const accountRouter = require('./account');
 const express = require('express');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/account', accountRouter)
 
 module.exports = router;
