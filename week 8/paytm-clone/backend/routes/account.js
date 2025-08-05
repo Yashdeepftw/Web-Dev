6121455a814b9e725a20e223dda90500b1b4a395
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-05 23:31:37 +0530

    added the account "/balance" route logic with transaction of db

--- week 8/paytm-clone/backend/routes/account.js
+++ week 8/paytm-clone/backend/routes/account.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { User, Account } = require('../db');
-const authMiddleware = require('../middlewares/middlewares')
+const {  Account } = require('../db');
+const  { authMiddleware } = require('../middlewares/middlewares')
 
 
-router.get('balance', authMiddleware, async (req, res) => {
+router.get('/balance', authMiddleware, async (req, res) => {
     const account = await Account.findOne({
         userId: req.userId
     });
@@ -14,6 +14,51 @@ router.get('balance', authMiddleware, async (req, res) => {
     })
 })
 
+router.post('/transfer', authMiddleware, async (req, res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({
+        userId: req.userId
+    }).session(session);
+
+    if(!account || account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg: 'Account not found or Insufficient Balance'
+        });
+    }
+
+    const toAccount = await Account.findOne({
+        userId: to
+    }).session(session);
+
+    if(!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg: 'account not found'
+        })
+    }
+
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: { balance: -amount}
+    }).session(session);
+    
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: { balance: amount}
+    }).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        msg: 'Transfer successful'
+    });
+});
 module.exports = {
     router
 }
