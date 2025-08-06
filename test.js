fdcf3a2baf8878ab65ae754bee2839ad85fabafa
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-07 01:44:57 +0530

    learned about the usage of axios and fetch and their defferences

--- test.js
+++ test.js
@@ -1,67 +1,25 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
-
-const app = express();
-app.use(express.json());
-
-const ALL_USERS = [
-  {
-    username: "harkirat@gmail.com",
-    password: "123",
-    name: "harkirat singh",
-  },
-  {
-    username: "raman@gmail.com",
-    password: "123321",
-    name: "Raman singh",
-  },
-  {
-    username: "priya@gmail.com",
-    password: "123321",
-    name: "Priya kumari",
-  },
-];
-
-function userExists(username, password) {
-  // write logic to return true or false if this user exists
-  // in ALL_USERS array
-  let userExists = false;
-  for (let i = 0; i < ALL_USERS.length; i++) {
-    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
-        userExists = true;
-    }
-  }
-  return userExists;
+const axios = require("axios");
+function main() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(async res => {
+      const json = await res.json();
+      console.log(json);
+    });
 }
 
-app.post("/signin", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  if (!userExists(username, password)) {
-    return res.status(403).json({
-      msg: "User doesnt exist in our in memory db",
-    });
-  }
-
-  var token = jwt.sign({ username: username }, jwtPassword);
-  return res.json({
-    token,
+async function main1() {
+  const res = await axios({
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    method: "GET",
+    headers: {
+      Authorization: "Bearer 23123",
+    },
+    data: {
+      username: "kirat"
+    }
   });
-});
-
-app.get("/users", function (req, res) {
-  const token = req.headers.authorization;
-  try {
-    const decoded = jwt.verify(token, jwtPassword);
-    const username = decoded.username;
-    // return a list of users other than this username
-  } catch (err) {
-    return res.status(403).json({
-      msg: "Invalid token",
-    });
-  }
-});
-
-app.listen(3000)
+  console.log(response)
+}
+main();
+main1();
