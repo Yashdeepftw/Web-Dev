65a5cc3350dfce1dff8bffa4e1c64fb8f7664146
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-09 01:23:55 +0530

    added the all components for the SignUp page

--- week 8/paytm-clone/frontend/src/pages/Signup.jsx
+++ week 8/paytm-clone/frontend/src/pages/Signup.jsx
@@ -1,6 +1,20 @@
+import { Heading } from "../components/Heading"
+import { SubHeading } from "../components/SubHeading"
+import { InputBox } from "../components/InputBox"
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
 
 export const Signup = () => {
     return <div>
-        
+        <div>
+            <Heading />
+            <SubHeading />
+            <InputBox /> 
+            <InputBox /> 
+            <InputBox /> 
+            <InputBox /> 
+            <Button /> 
+            <BottomWarning />
+        </div>
     </div>
 }
