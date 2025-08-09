66c8496c135372d01a81897322c8bbad6be284db
unknown <yashdeepchauhan295@gmail.com>
2025-08-10 00:03:17 +0530

    added the sign in and sign up pages

--- week 8/paytm-clone/frontend/src/pages/Signup.jsx
+++ week 8/paytm-clone/frontend/src/pages/Signup.jsx
@@ -5,16 +5,20 @@ import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 
 export const Signup = () => {
-    return <div>
-        <div>
-            <Heading />
-            <SubHeading />
-            <InputBox /> 
-            <InputBox /> 
-            <InputBox /> 
-            <InputBox /> 
-            <Button /> 
-            <BottomWarning />
+    return <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="flex flex-cols justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                <Heading label={'Sign up'} />
+                <SubHeading label={'Enter your infomation to create an account'} />
+                <InputBox placeholder="John" label={"First Name"} /> 
+                <InputBox placeholder='Doe' label={'Last Name'} /> 
+                <InputBox placeholder='example@gmail.com' label={"Email"} /> 
+                <InputBox placeholder='12345' label={'Password'} />
+                <div className="pt-4">
+                    <Button label={'Sign Up'} /> 
+                </div> 
+                <BottomWarning label={'Already have an account?'} buttonText={'Sing in'} to={'/signin'} />
+            </div>
         </div>
     </div>
 }
