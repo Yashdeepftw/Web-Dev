b8db59337bbe4df70dd2873d03a57e1f8e00509f
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-19 00:58:00 +0530

    added the custom hooks example

--- week 9/hooks/src/App.jsx
+++ week 9/hooks/src/App.jsx
@@ -2,35 +2,65 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [render, setRender] = useState(true);
+function useTodos() {
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
-    setInterval(() => {
-      setRender(render => !render);
-    }, 1000)
-  }, []);
-  if(!render) {
-    return <div style={{ background: 'red', height: '100px' }}>visible div</div>
-  }
-  return (
-    <>
-      <Mycomponent />
-    </>
-  )
+    axios.get("http://localhost:3000")
+      .then((res) => {
+        setTodos(res.data.todos);
+      })
+  }, [])
+
+  return todos;
 }
 
-function Mycomponent () {
-  useEffect(() => {
-    console.error('component mounted');
-
-    return () => {
-      console.log('component unmounted');
-    }
-  }, []);
-
+function Track({ todos }) {
   return <div>
-    from the inside of the element
+    {todos.title} 
+    <br />
+    {todos.description}
   </div>
+}
+
+function App() {
+
+  const todos = useTodos();
+
+  return (
+    <>
+      {todos.map(todo => <Track todos={todo} />)}
+    </>
+  )
+
+//   const [render, setRender] = useState(true);
+//   useEffect(() => {
+//     setInterval(() => {
+//       setRender(render => !render);
+//     }, 1000)
+//   }, []);
+//   if(!render) {
+//     return <div style={{ background: 'red', height: '100px' }}>visible div</div>
+//   }
+//   return (
+//     <>
+//       <Mycomponent />
+//     </>
+//   )
+// }
+
+// function Mycomponent () {
+//   useEffect(() => {
+//     console.error('component mounted');
+
+//     return () => {
+//       console.log('component unmounted');
+//     }
+//   }, []);
+
+//   return <div>
+//     from the inside of the element
+//   </div>
 }
 
 export default App
