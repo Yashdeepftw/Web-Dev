6e18527d3146d052de5cf7da103809a59f51108b
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-18 22:30:10 +0530

    added the week 9.1 class basic

--- week 9/hooks/src/App.jsx
+++ week 9/hooks/src/App.jsx
@@ -1,13 +1,36 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [render, setRender] = useState(true);
+  useEffect(() => {
+    setInterval(() => {
+      setRender(render => !render);
+    }, 1000)
+  }, []);
+  if(!render) {
+    return <div style={{ background: 'red', height: '100px' }}>visible div</div>
+  }
   return (
     <>
-      
+      <Mycomponent />
     </>
   )
+}
+
+function Mycomponent () {
+  useEffect(() => {
+    console.error('component mounted');
+
+    return () => {
+      console.log('component unmounted');
+    }
+  }, []);
+
+  return <div>
+    from the inside of the element
+  </div>
 }
 
 export default App
