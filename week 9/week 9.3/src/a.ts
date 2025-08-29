ada1251f423601c51fda23ea5738102486dfc47f
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-30 00:24:36 +0530

    completed the week 9.3 class

--- week 9/week 9.3/src/a.ts
+++ week 9/week 9.3/src/a.ts
@@ -17,8 +17,16 @@
 
 // Generics
 
-type Input = number | string;
+type Input = [] | number | string;
 
 function firstEl (arr: Input[]) {
     return arr[0];
 }
+
+function identity<T>(args: T): T {
+    return args;
+}
+
+let val1 = identity<string>('singh');
+let val2 = identity<number>(2)
+val1.toUpperCase();
