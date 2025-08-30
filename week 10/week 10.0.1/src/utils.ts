b15492e2741f0963192e681aed44e5988583afa5
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-08-31 00:01:41 +0530

    added the postgresSql connection link and connected the DB to the code

--- week 10/week 10.0.1/src/utils.ts
+++ week 10/week 10.0.1/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgressql://postgres:yashdeep_0218@localhost:5432/Todos");
     await client.connect();
     return client;
 }
