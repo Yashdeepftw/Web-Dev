836e68df40407836defbc8d712de68b1fd5b0ee0
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-09-02 01:08:42 +0530

    added some function code in the file

--- week 10/week 10.0.2/src/04-get-user.ts
+++ week 10/week 10.0.2/src/04-get-user.ts
@@ -7,15 +7,15 @@ const prisma = new PrismaClient()
 async function main() {
     const users = await prisma.user.findMany({});
     console.log(users);
-    const user = await prisma.user.findUnique({
-        where: {
-            id: 1
-        },
-        include: {
-            posts: true
-        }
-    });
-    console.log(user);
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1
+    //     },
+    //     include: {
+    //         posts: true
+    //     }
+    // });
+    // console.log(user);
 };
 
 main()
