60f944cc70239bec4454b9bf4bdde4f480b6151c
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-09-14 01:08:56 +0530

    added the prisma schema and signup route is completed

--- week 13/Medium/backend/src/index.ts
+++ week 13/Medium/backend/src/index.ts
@@ -1,6 +1,15 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt';
 
-const app = new Hono()
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
 
 app.get('/api/v1/blog/:id', (c) => {
   const id = c.req.param('id');
@@ -8,10 +17,33 @@ app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/signup', (c) => {
-  return c.json({
-    msg: 'signup page'
-  })
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    })
+  
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+
+    return c.json({
+      msg: 'signup page',
+      jwt
+    });
+  }
+  catch(e) {
+    c.status(403);
+    console.log(e);
+    return c.json({error: 'error while signup'});
+  }
 });
 
 app.post('/api/v1/signin', (c) => {
