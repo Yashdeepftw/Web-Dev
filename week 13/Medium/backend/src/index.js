a6a28563a1a5ffe2d0fb015c326d4c2166cb041f
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-09-13 12:42:25 +0530

    added the week 13 medium project template with backend routes using HONO

---
+++ week 13/Medium/backend/src/index.js
@@ -0,0 +1,28 @@
+import { Hono } from 'hono';
+const app = new Hono();
+app.get('/api/v1/blog/:id', (c) => {
+    const id = c.req.param('id');
+    console.log(id);
+    return c.text('Hello Hono!');
+});
+app.post('/api/v1/signup', (c) => {
+    return c.json({
+        msg: 'signup page'
+    });
+});
+app.post('/api/v1/signin', (c) => {
+    return c.json({
+        msg: 'signin page'
+    });
+});
+app.post('/api/v1/blog', (c) => {
+    return c.json({
+        msg: 'blog page'
+    });
+});
+app.put('/api/v1/blog', (c) => {
+    return c.json({
+        msg: 'blog put page'
+    });
+});
+export default app;
