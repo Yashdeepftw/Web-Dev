807cae2fbdb8410e280c0dbb62010f522fae2478
SHRUTI CHAUHAN <Yashdeepchauhan295@gmail.com>
2025-09-26 23:08:06 +0530

    added some code of the class

--- week 14/my-app2/app/page.tsx
+++ week 14/my-app2/app/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default async function  Home() {
   async function getUserDetails() {
-    const response = await axios.get("");
+    const response = await axios.get("http://localhost:3001/api/user");
     return response.data;
   }
   const userData = await getUserDetails();
---
+++ week 14/my-app2/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default  function () {
+    return (
+        <div>
+            <input type="text" placeholder="name" />
+            <input type="text" placeholder="email"/>
+            <button>Signup</button>
+        </div>
+    )
+}
